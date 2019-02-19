4640a5483102df8cfc4bfd592bf181140fb481f8
jamesijw23 <jamesijw23@gmail.com>
2019-02-19 05:47:29 -0800

    Added new file because of Error

--- R/summarize_anova_1.R
+++ R/summarize_anova_1.R
@@ -8,14 +8,14 @@
 
 summarize_anova = function(df, x_variable, y_variable){
 
-  df2 = dplyr::select(df,x_variable, y_variable)
+  df2 = dplyr::select(df, x_variable, y_variable)
 
   colnames(df2) = c('v1','v2')
   df2$v1 = as.factor(df2$v1)
 
   sum_table = dplyr::group_by(df2,v1)
   sum_table = dplyr::summarise(sum_table,
-                               sample_size_by_group = n(),
+                               sample_size_by_group = dplyr::n(),
                                mean_by_group = mean(v2),
                                sd_by_group = sd(v2))
 
