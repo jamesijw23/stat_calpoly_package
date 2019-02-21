04c61d09cb0687787bcfca73d40b57844904fd2c
jamesijw23 <jamesijw23@gmail.com>
2019-02-21 06:42:01 -0800

    Adding more data

---
+++ R/fx.R
@@ -0,0 +1,9 @@
+#' first function fun
+#' @param radian
+#' @return value
+#' @export
+
+
+fx =function(x){
+  return(x^2)
+}
---
+++ R/summarize_slr_.R
@@ -0,0 +1,23 @@
+#' summarize_slr_
+#' @param radian @name ss slr @description
+#' @return value
+#' @export
+
+
+
+
+
+summarize_slr_ = function(df, x_variable, y_variable){
+
+  df2 = dplyr::select(df, x_variable, y_variable)
+
+  colnames(df2) = c('x','y')
+  sum_table = dplyr::summarise(df2,sample_size = n(),
+                               mean_x = mean(x),
+                               sd_x = sd(x),
+                               mean_y = mean(y),
+                               sd_y = sd(y),
+                               cor_xy =cor(x,y))
+
+  return(sum_table)
+}
