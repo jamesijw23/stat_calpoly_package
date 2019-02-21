55ac2e2ffc8ada1e9282541b61749c49967d976c
jamesijw23 <jamesijw23@gmail.com>
2019-02-21 10:53:34 -0800

    time series plot

---
+++ R/timeseriesplot.R
@@ -0,0 +1,15 @@
+#' Graph scatter_plot
+#' @param radian @name scatter_plot @description provides an easy way to view if data is norma;
+#' @return value
+#' @export
+#' 
+ 
+
+time_series_plot = function(df,stock_date, x_variable, line_color = "red",main_title){
+ggplot2::ggplot(df, ggplot2::aes(stock_date, x_variable)) + 
+  ggplot2::geom_line(color = ) +
+  ggplot2::theme_bw() +
+  ggplot2::ggtitle(main_title) +
+  ylab("Stock Price") +
+  ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5,size = 15, face = "bold"))
+}
