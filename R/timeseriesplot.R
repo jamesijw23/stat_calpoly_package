db7a9ff035e3c16d10e0e680a3fa601439b50ceb
jamesijw23 <jamesijw23@gmail.com>
2019-02-22 11:00:06 -0800

    Added data and timeseries plot

--- R/timeseriesplot.R
+++ R/timeseriesplot.R
@@ -5,11 +5,12 @@
 #'
 
 
-time_series_plot = function(df,stock_date, x_variable, line_color = "red",main_title){
-ggplot2::ggplot(df, ggplot2::aes_string(stock_date, x_variable)) +
-  ggplot2::geom_line(color = line_color) +
-  ggplot2::theme_bw() +
-  ggplot2::ggtitle(main_title) +
-  ylab("Stock Price") +
-  ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5,size = 15, face = "bold"))
+time_series_plot = function(df, y_variable, line_color = "red",main_title){
+  ggplot2::ggplot(df, ggplot2::aes_string(x = "stock_date", y = y_variable)) +
+    ggplot2::geom_line(color = line_color) +
+    ggplot2::theme_bw() +
+    ggplot2::ggtitle(main_title) +
+    ggplot2::ylab(paste0(y_variable," Stock Price")) +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5,size = 15, face = "bold"))
+  
 }
