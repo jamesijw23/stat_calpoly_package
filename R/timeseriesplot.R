7d6122de10803e3d7aba7e7c4792627d7a223bd5
jamesijw23 <jamesijw23@gmail.com>
2019-02-26 13:20:33 -0800

    added time series complexity and titanic data modidication

--- R/timeseriesplot.R
+++ R/timeseriesplot.R
@@ -5,11 +5,16 @@
 #'
 
 
-time_series_plot = function(df, y_variable, line_color = "red",main_title){
+time_series_plot = function(df, y_variable, line_color = "red",main_title, start_date,end_date,by_n = 1){
+  s_d = as.Date(start_date)
+  e_d = as.Date(end_date)
+  
+  
+  df = dplyr::filter(df,stock_date %in% seq(s_d, e_d,by_n))
   ggplot2::ggplot(df, ggplot2::aes_string(x = "stock_date", y = y_variable)) +
     ggplot2::geom_line(color = line_color) +
     ggplot2::theme_bw() +
-    ggplot2::ggtitle(main_title) +
+    ggplot2::ggtitle(paste0(main_title,' from ',s_d,' to ',e_d,sep='')) +
     ggplot2::ylab(paste0(y_variable," Stock Price")) +
     ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5,size = 15, face = "bold"))
   
