56ec76ddacf5330903159e549d53b5eb527ffe7f
jamesijw23 <jamesijw23@gmail.com>
2020-02-11 10:28:01 -0800

    New slr function that works

---
+++ R/summarize_slr_info.R
@@ -0,0 +1,23 @@
+#' summarize_slr
+#' @param radian @name ss slr @description
+#' @return value
+#' @export
+
+
+
+
+summarize_slr = function(df, x_variable, y_variable){
+
+  df2 = dplyr::select(df, x_variable, y_variable)
+
+  colnames(df2) = c('x','y')
+  sum_table = dplyr::summarise(df2,sample_size = n(),
+                     mean_x = mean(x,na.rm = T),
+                     sd_x = sd(x,na.rm = T),
+                     mean_y = mean(y,na.rm = T),
+                     sd_y = sd(y,na.rm = T),
+                     cor_xy =cor(x,y, use="complete.obs"))
+
+  return(sum_table)
+}
+
