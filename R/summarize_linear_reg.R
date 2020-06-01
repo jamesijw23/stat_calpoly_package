9d64ceaba5099717c09973d1b4dccac961b7b925
jamesijw23 <jamesijw23@gmail.com>
2020-06-01 15:32:41 -0700

    count being shown

--- R/summarize_linear_reg.R
+++ R/summarize_linear_reg.R
@@ -12,7 +12,7 @@ summarize_linear_reg = function(df, x_variable, y_variable){
   df2 = dplyr::select(df, x_variable, y_variable)
 
   colnames(df2) = c('x','y')
-  sum_table = dplyr::summarise(df2,sample_size = n(),
+  sum_table = dplyr::summarise(df2,sample_size = dplyr::n(),
                      mean_x = mean(x,na.rm = T),
                      sd_x = sd(x,na.rm = T),
                      mean_y = mean(y,na.rm = T),
